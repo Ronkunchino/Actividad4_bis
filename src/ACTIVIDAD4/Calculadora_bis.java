a863001eaddaa374e314e94ef46593278b59ead0
RobertoHevia <robertohevia@ejemplo.es>
2023-03-30 01:02:50 +0200

    He refactorizado las diferentes operaciones creando métodos

--- src/ACTIVIDAD4/Calculadora_bis.java
+++ src/ACTIVIDAD4/Calculadora_bis.java
@@ -2,7 +2,7 @@ package ACTIVIDAD4;
 
 /**
 * <h2> Clase Calculadora, se utiliza para realizar las operaciones básicas: suma, resta, multiplicación y división</h2>.
-* @version 2-(27-3-2023)
+* @version 3-(27-3-2023)
 * @author Roberto Hevia Muñiz
 * @since Versión 0 del programa
 */
@@ -12,37 +12,81 @@ public class Calculadora_bis
 
 	public static void main(String[] args) 
 	{
-			//Declaro las variables
-	       	int numero1;
-	       	int numero2;
-	       	int suma, resta, multiplicacion, division;
+		//Declaro las variables
+       	int numero1;
+       	int numero2;
+       	//int suma, resta, multiplicacion, division;
+       
+       	// Pido al usuario que introduzca un número
+        System.out.print("Introduzca un número: ");
+        numero1 = Teclado.entero();
+        
+        //Pido al usuario que introduzca un segundo número para realizar las operaciones
+        System.out.print("Introduzca otro número: ");
+        numero2 = Teclado.entero();
+        
+        suma(numero1, numero2);
+        
+        resta(numero1, numero2);
+        
+        multiplicacion(numero1, numero2);
+        
+        division(numero1, numero2);
+	}
 
+	/**
+	 * @param numero1
+	 * @param numero2
+	 * @author Roberto Hevia Muñiz
+	 * @since Versión 1 del programa
+	 */
+	public static void division(int numero1, int numero2) 
+	{
+		int division;
+		//Instrucciones para realizar la división
+			division = numero1 / numero2;
+			System.out.println("La división de " + numero1 + " / " + numero2 + " es: " + division);
+	}
 
-	       	// Pido al usuario que introduzca un número
-	        System.out.print("Introduzca un número: ");
-	        numero1 = Teclado.entero();
+	/**
+	 * @param numero1
+	 * @param numero2
+	 * @author Roberto Hevia Muñiz
+	 * @since Versión 1 del programa
+	 */
+	public static void multiplicacion(int numero1, int numero2) 
+	{
+		int multiplicacion;
+		//Instrucciones para realizar la multiplicación
+			multiplicacion = numero1 * numero2;
+			System.out.println("La multiplicación de " + numero1 + " * " + numero2 + " es: " + multiplicacion);
+	}
 
-
-	        //Pido al usuario que introduzca un segundo número para realizar las operaciones
-	        System.out.print("Introduzca otro número: ");
-	        numero2 = Teclado.entero();
-
-
-	        //Instrucciones para realizar la suma
-	        suma = numero1 + numero2;
-	        System.out.println("La suma de " + numero1 + " + " + numero2 + " es: " + suma);
-
-	      //Instrucciones para realizar la resta
+	/**
+	 * @param numero1
+	 * @param numero2
+	 * @author Roberto Hevia Muñiz
+	 * @since Versión 1 del programa
+	 */
+	public static void resta(int numero1, int numero2) 
+	{
+		int resta;
+		//Instrucciones para realizar la resta
 	        resta = numero1 - numero2;
 	        System.out.println("La resta de " + numero1 + " - " + numero2 + " es: " + resta);
+	}
 
-	      //Instrucciones para realizar la multiplicación
-	        multiplicacion = numero1 * numero2;
-	        System.out.println("La multiplicación de " + numero1 + " * " + numero2 + " es: " + multiplicacion);
-
-	      //Instrucciones para realizar la división
-	        division = numero1 / numero2;
-	        System.out.println("La división de " + numero1 + " / " + numero2 + " es: " + division);	
-	  }
-
+	/**
+	 * @param numero1
+	 * @param numero2
+	 * @author Roberto Hevia Muñiz
+	 * @since Versión 0 del programa
+	 */
+	public static void suma(int numero1, int numero2) 
+	{
+		int suma;
+		//Instrucciones para realizar la suma
+		suma = numero1 + numero2;
+		System.out.println("La suma de " + numero1 + " + " + numero2 + " es: " + suma);
+	}
 }
